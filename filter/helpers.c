84ce98ec03f973e936ef4ef1ce0fa0429f457a59
Violeta Petrova <59619633+r10d10@users.noreply.github.com>
2020-03-26 22:39:08 +0200

    working blur

--- filter/helpers.c
+++ filter/helpers.c
@@ -47,7 +47,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    int sumR, sumG, sumB, d;
+    float sumR, sumG, sumB, d;
     RGBTRIPLE temp[height][width];
 
     //makes an copie of the image
@@ -76,185 +76,28 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 // finds the sum of the next blocks to each pixel
                 for (int jj = -1; jj < 2; jj++)
                 {
-                    if ((i + ii >= 0) && (i + ii < height) && (j + jj >= 0) && (j + jj < width))
+                    if ((i + ii >= 0) && (i + ii < height) && (j + jj >= 0)
+                    && (j + jj < width))
                     {
-                        sumR += temp[i + ii][j + jj].rgbtRed;
-                        sumG += temp[i + ii][j + jj].rgbtGreen;
-                        sumB += temp[i + ii][j + jj].rgbtBlue;
+                        sumR += (float)temp[i + ii][j + jj].rgbtRed;
+                        sumG += (float)temp[i + ii][j + jj].rgbtGreen;
+                        sumB += (float)temp[i + ii][j + jj].rgbtBlue;
 
                         d++;
                     }
                 }
             }
-                image[i][j].rgbtBlue = round(sumB / d);
-                image[i][j].rgbtRed = round(sumR / d);
-                image[i][j].rgbtGreen = round(sumG / d);
+                image[i][j].rgbtBlue = (BYTE)round(sumB / (float)d);
+                image[i][j].rgbtRed = (BYTE)round(sumR / (float)d);
+                image[i][j].rgbtGreen = (BYTE)round(sumG / (float)d);
 
         }
     }
     return;
 }
+
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    int sumXR, sumXG, sumXB, d, sumYR, sumYG, sumYB;
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if ((i == 0) && (j == 0))
-            {
-                sumXB = (image[i + 1][j + 1].rgbtBlue) + (2 * image[i + 1][j].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                sumXR = (image[i + 1][j + 1].rgbtRed) + (2 * image[i + 1][j].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                sumXG = (image[i + 1][j + 1].rgbtGreen) + (2 * image[i + 1][j].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                // Sobel operator - Gy
-                sumYB = (image[i + 1][j + 1].rgbtBlue) + (2 * image[i][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                sumYR = (image[i + 1][j + 1].rgbtRed) + (2 * image[i][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                sumYG = (image[i + 1][j + 1].rgbtGreen) + (2 * image[i][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-                break;
-            }
-
-            if ((i + 1 == height) && (j + 1 == width))
-            {
-                // Sobel operator - Gx
-                sumXB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i - 1][j].rgbtBlue);
-                sumXR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i - 1][j].rgbtRed);
-                sumXG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i - 1][j].rgbtGreen);
-
-                // Sobel operator - Gy
-                sumYB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i][j - 1].rgbtBlue);
-                sumYR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i][j - 1].rgbtRed);
-                sumYG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i][j - 1].rgbtGreen);
-                break;
-            }
-
-
-            if ((i == 0) && (j != 0))
-            {
-                sumXB = (image[i + 1][j + 1].rgbtBlue) + (2 * image[i + 1][j].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                sumXR = (image[i + 1][j + 1].rgbtRed) + (2 * image[i + 1][j].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                sumXG = (image[i + 1][j + 1].rgbtGreen) + (2 * image[i + 1][j].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                // Sobel operator - Gy
-                sumYB = (image[i + 1][j + 1].rgbtBlue) + (-2 * image[i][j - 1].rgbtBlue) + (2 * image[i][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                sumYR = (image[i + 1][j + 1].rgbtRed) + (-2 * image[i][j - 1].rgbtRed) + (2 * image[i][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                sumYG = (image[i + 1][j + 1].rgbtGreen) + (-2 * image[i][j - 1].rgbtGreen) + (2 * image[i][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-            }
-            else
-            {
-                if ((i != 0) && (j == 0))
-                {
-                    // Sobel operator - Gx
-                    sumXB = (image[i + 1][j + 1].rgbtBlue) + (-2 * image[i - 1][j].rgbtBlue) + (2 * image[i + 1][j].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                    sumXR = (image[i + 1][j + 1].rgbtRed) + (-2 * image[i - 1][j].rgbtRed) + (2 * image[i + 1][j].rgbtRed) + (-image[i - 1][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                    sumXG = (image[i + 1][j + 1].rgbtGreen) + (-2 * image[i - 1][j].rgbtGreen) + (2 * image[i + 1][j].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                    // Sobel operator - Gy
-                    sumYB = (image[i + 1][j + 1].rgbtBlue) + (2 * image[i][j + 1].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                    sumYR = (image[i + 1][j + 1].rgbtRed) + (2 * image[i][j + 1].rgbtRed) + (-image[i - 1][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                    sumYG = (image[i + 1][j + 1].rgbtGreen) + (2 * image[i][j + 1].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                }
-                else
-                {
-                    if ((i + 1 == height) && (j + 1 != width))
-                    {
-                         // Sobel operator - Gx
-                        sumXB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i - 1][j].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue);
-                        sumXR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i - 1][j].rgbtRed) + (-image[i - 1][j + 1].rgbtRed);
-                        sumXG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i - 1][j].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen);
-
-                        // Sobel operator - Gy
-                        sumYB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i][j - 1].rgbtBlue) + (2 * image[i][j + 1].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue);
-                        sumYR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i][j - 1].rgbtRed) + (2 * image[i][j + 1].rgbtRed) + (-image[i - 1][j + 1].rgbtRed);
-                        sumYG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i][j - 1].rgbtGreen) + (2 * image[i][j + 1].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen);
-                    }
-                    else
-                    {
-                        if ((i + 1 != height) && (j + 1 == width))
-                        {
-                            // Sobel operator - Gx
-                            sumXB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i - 1][j].rgbtBlue) + (2 * image[i + 1][j].rgbtBlue);
-                            sumXR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i - 1][j].rgbtRed) + (2 * image[i + 1][j].rgbtRed);
-                            sumXG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i - 1][j].rgbtGreen) + (2 * image[i + 1][j].rgbtGreen);
-
-                             // Sobel operator - Gy
-                            sumYB = (-image[i - 1][j - 1].rgbtBlue) + (-2 * image[i][j - 1].rgbtBlue);
-                            sumYR = (-image[i - 1][j - 1].rgbtRed) + (-2 * image[i][j - 1].rgbtRed);
-                            sumYG = (-image[i - 1][j - 1].rgbtGreen) + (-2 * image[i][j - 1].rgbtGreen);
-
-                        }
-                        else
-                        {
-                            // Sobel operator - Gx
-                            sumXB = (-image[i - 1][j - 1].rgbtBlue) + (image[i + 1][j + 1].rgbtBlue) + (-2 * image[i - 1][j].rgbtBlue) + (2 * image[i + 1][j].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                            sumXR = (-image[i - 1][j - 1].rgbtRed) + (image[i + 1][j + 1].rgbtRed) + (-2 * image[i - 1][j].rgbtRed) + (2 * image[i + 1][j].rgbtRed) + (-image[i - 1][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                            sumXG = (-image[i - 1][j - 1].rgbtGreen) + (image[i + 1][j + 1].rgbtGreen) + (-2 * image[i - 1][j].rgbtGreen) + (2 * image[i + 1][j].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                            // Sobel operator - Gy
-                            sumYB = (-image[i - 1][j - 1].rgbtBlue) + (image[i + 1][j + 1].rgbtBlue) + (-2 * image[i][j - 1].rgbtBlue) + (2 * image[i][j + 1].rgbtBlue) + (-image[i - 1][j + 1].rgbtBlue) + ( image[i + 1][j + 1].rgbtBlue);
-                            sumYR = (-image[i - 1][j - 1].rgbtRed) +  (image[i + 1][j + 1].rgbtRed) + (-2 * image[i][j - 1].rgbtRed) + (2 * image[i][j + 1].rgbtRed) + (-image[i - 1][j + 1].rgbtRed) + ( image[i + 1][j + 1].rgbtRed);
-                            sumYG = (-image[i - 1][j - 1].rgbtGreen) + (image[i + 1][j + 1].rgbtGreen) + (-2 * image[i][j - 1].rgbtGreen) + (2 * image[i][j + 1].rgbtGreen) + (-image[i - 1][j + 1].rgbtGreen) + ( image[i + 1][j + 1].rgbtGreen);
-
-                        }
-                    }
-                }
-            }
-
-            d = sqrt(sumXB * sumXB + sumYB * sumYB);
-            if (d <= 0)
-            {
-                image[i][j].rgbtBlue = 0;
-            }
-            else
-            {
-                if (d >= 255)
-                {
-                    image[i][j].rgbtBlue = 255;
-                }
-                else
-                {
-                    image[i][j].rgbtBlue = d;
-                }
-            }
-
-
-            d = sqrt(sumXG * sumXG + sumYG * sumYG);
-            if (d <= 0)
-            {
-                image[i][j].rgbtGreen = 0;
-            }
-            else
-            {
-                if (d >= 255)
-                {
-                    image[i][j].rgbtGreen = 255;
-                }
-                else
-                {
-                    image[i][j].rgbtGreen = d;
-                }
-            }
-
-
-            d = sqrt(sumXR * sumXR + sumYR * sumYR);
-            if (d <= 0)
-            {
-                image[i][j].rgbtRed = 0;
-            }
-            else
-            {
-                if (d >= 255)
-                {
-                    image[i][j].rgbtRed = 255;
-                }
-                else
-                {
-                    image[i][j].rgbtRed = d;
-                }
-            }
-        }
-    }
-    return;
+    
 }
